e496786924926651db96fb9db0d52208fd6f2d18
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-17 14:27:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 def my_each(arr) # put argument(s) here
   n = 0
-  while i < arr.length
+  while n < arr.length
 #     # Will yield the specific iteration to a seperate block
    yield(arr[i])
 #     # Increments up by one
-   i += 1
+   n += 1
    arr.map {|i|}
 end
 arr
